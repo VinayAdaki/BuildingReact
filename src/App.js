2d72efb62dacd1a03fb93d849298ef535b95038a
Vinay Adaki <vinay.adki123@gmail.com>
2018-05-03 08:59:07 +0530

    feat: added Delete project feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
 import Projects from './Components/Projects';
 import AddProject from './Components/AddProject';
 import './App.css';
@@ -15,14 +16,17 @@ class App extends Component {
     this.setState({
       projects: [
         {
+          id: uuid.v4(),
           title: 'Business Website',
           category: 'Web Design'
         },
         {
+          id: uuid.v4(),
           title: 'Social App',
           category: 'Mobile Development'
         },
         {
+          id: uuid.v4(),
           title: 'Ecommarce shopping Cart',
           category: 'Web Development'
         }
@@ -30,16 +34,23 @@ class App extends Component {
     });
   }
   handleAddProject(project){
-    let projects = this.state.projects;
-    projects.push(project);
-    this.setState({projects:projects});
+      let projects = this.state.projects;
+      projects.push(project);
+      this.setState({projects});
+  }
+
+  handleDeleteProject(id){
+      let projects = this.state.projects;
+      let index = projects.findIndex(project => project.id === id);
+      projects.splice(index, 1);
+      this.setState({projects});
   }
 
   render() {
     return (
       <div className="App">
         <AddProject addProject={this.handleAddProject.bind(this)}/>
-        <Projects projects={this.state.projects}/>
+        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
       </div>
     );
   }
--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import uuid from 'uuid';
 
 class AddProject extends Component {
   constructor(){
@@ -16,6 +18,7 @@ class AddProject extends Component {
           alert('Title is required');
       } else {
           this.setState({newProject: {
+            id: uuid.v4(),
             title: this.refs.title.value,
             category: this.refs.category.value
           }
@@ -45,11 +48,18 @@ class AddProject extends Component {
               {categoryOptions}
             </select>
           </div>
+          <br />
           <input type="submit" value="Submit"/>
+          <br/>
         </form>
       </div>
     );
   }
+}
+
+AddProject.propTypes = {
+  categories: PropTypes.array,
+  addProject: PropTypes.func
 }
 
 export default AddProject;
