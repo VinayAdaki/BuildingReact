6bb94e63888a63dedb5999a87980920589d8ce4f
va04939 <AdakiVinay@johndeere.com>
2018-05-05 16:39:05 +0530

    refactore: alignment and inline export

--- src/App.js
+++ src/App.js
@@ -1,54 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import uuid from 'uuid';
 import Projects from './Components/Projects';
 import AddProject from './Components/AddProject';
 import './App.css';
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      projects: [
-        {
-          id: uuid.v4(),
-          title: 'Business Website',
-          category: 'Web Design'
-        },
-        {
-          id: uuid.v4(),
-          title: 'Social App',
-          category: 'Mobile Development'
-        },
-        {
-          id: uuid.v4(),
-          title: 'Ecommarce shopping Cart',
-          category: 'Web Development'
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            projects: [
+                {
+                    id: uuid.v4(),
+                    title: 'Business Website',
+                    category: 'Web Design'
+                },
+                {
+                    id: uuid.v4(),
+                    title: 'Social App',
+                    category: 'Mobile Development'
+                },
+                {
+                    id: uuid.v4(),
+                    title: 'Ecommarce shopping Cart',
+                    category: 'Web Development'
+                }
+            ]
         }
-      ]
     }
-  }
 
-  handleAddProject(project){
-      let projects = this.state.projects;
-      projects.push(project);
-      this.setState({projects});
-  }
+    handleAddProject(project) {
+        let projects = this.state.projects;
+        projects.push(project);
+        this.setState({projects});
+    }
 
-  handleDeleteProject(id){
-      let projects = this.state.projects;
-      const index = projects.findIndex(project => project.id === id);
-      projects.splice(index, 1);
-      this.setState({projects});
-  }
+    handleDeleteProject(id) {
+        let projects = this.state.projects;
+        const index = projects.findIndex(project => project.id === id);
+        projects.splice(index, 1);
+        this.setState({projects});
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <AddProject addProject={this.handleAddProject.bind(this)}/>
-        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <AddProject addProject={this.handleAddProject.bind(this)}/>
+                <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
+            </div>
+        );
+    }
 }
-
-export default App;
